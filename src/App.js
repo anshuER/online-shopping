b317488423584f8558fdae6148070e377685c1ed
anshu patel <anshupatel710@gmail.com>
2022-03-22 14:57:06 +0530

    added redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route ,Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+
 
 import './App.css';
 import Header from './components/header/header.component';
@@ -8,21 +10,24 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/pages/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 // import ShopPage from './pages/shop/shop.component.jsx';
-
+import {setCurrentUser} from './redux/user/user.actions'
 
 
 class App extends React.Component {
-  constructor()
-  {
-    super();
-    this.state={
-      currentUser:null
-    }
-  }
+  // constructor()
+  // {
+  //   super();
+  //   this.state={
+  //     currentUser:null
+  //   }
+  // }
 
   unsubscribeFromAuth=null;
 
   componentDidMount(){
+
+    const {setCurrentUser}=this.props;
+
     this.unsubscribeFromAuth= auth.onAuthStateChanged(async userAuth=>
       {
         if(userAuth)
@@ -30,38 +35,52 @@ class App extends React.Component {
           const userRef=await createUserProfileDocument(userAuth);
           userRef.onSnapshot(snapShot=>
             {
-              this.setState({
-                currentUser:{ 
+              // this.setState({
+              //   currentUser:{
+              //     id:snapShot.id,
+              //     ...snapShot.data()
+              //   }
+              // });
+
+              setCurrentUser({
+                
                   id:snapShot.id,
                   ...snapShot.data()
-                }
+                
               });
+
             });
         }
-        this.setState({currentUser:userAuth});
-        // this.setState({currentUser:user});   
+        setCurrentUser(userAuth );
+        // this.setState({currentUser:userAuth});
+        // this.setState({currentUser:user});
 
         //  createUserProfileDocument(user);
       })
   }
 
   componentWillUnmount(){
-    this.unsubscribeFromAuth(); 
+    this.unsubscribeFromAuth();
   }
 
   render() {
 
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+         <Header  />
+        {/* <Header currentUser={this.state.currentUser} /> */}
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </div>
-    ); 
+    );
   }
 }
 
-export default App;
+const mapDispatchToProps=dispatch=>({
+  setCurrentUser:user=>dispatch(setCurrentUser(user))
+})
+
+export default connect(null,mapDispatchToProps)(App);
